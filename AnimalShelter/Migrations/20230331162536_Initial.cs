d3623f131e69d2a15f74045f36aa6d9ffccfcb3b
David Jandron <jandron1211@gmail.com>
2023-03-31 09:26:35 -0700

    ADD model completion and database migration

---
+++ AnimalShelter/Migrations/20230331162536_Initial.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AnimalShelter.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Animals",
+                columns: table => new
+                {
+                    AnimalId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Breed = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Name = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    FunFact = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Animals", x => x.AnimalId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.InsertData(
+                table: "Animals",
+                columns: new[] { "AnimalId", "Age", "Breed", "FunFact", "Name" },
+                values: new object[,]
+                {
+                    { 1, 3, "Cat", "Naps a lot. Loves staring at birds. Speaks English", "Muffin" },
+                    { 2, 1, "Cat", "Smells like Drakar Noir. Cuddly, Hairless.", "Stinky" },
+                    { 3, 40, "Cat", "Actually human, but pretends to be a cat. Could use a nice home, psychiatrist, and medication.", "Steve" },
+                    { 4, 6, "Dog", "Poops chocolate. Know how to code in C#. Allergic to water", "Zorthrax" },
+                    { 5, 2, "Dog", "Sings beautifully. Crows like a rooster. Is a delivery driver for Amazon", "The Dog Formerly Known as Prince" },
+                    { 6, 1, "Tiger", "Previous owner was terrible at animal identification. Gluten-free. Flys", "Lion" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Animals");
+        }
+    }
+}
--- AnimalShelter/Models/AnimalShelterContext.cs
+++ AnimalShelter/Models/AnimalShelterContext.cs
@@ -21,16 +21,18 @@ namespace AnimalShelter.Models
     {
       builder.Entity<Animal>()
         .HasData(
-          new Animal { AnimalId = 1, Breed = "Cat", Name = "Muffin", Age = 3, FunFact = "Naps a lot. Loves staring at birds. Speaks English"},
+          new Animal { AnimalId = 1, Breed = "Cat", Name = "Muffin", Age = 3, FunFact = "Naps a lot. Loves staring at birds. Speaks English" },
           
-        //   new Planet { PlanetId = 2, Name = "Venus", FunFact = "Venus has no moons. A block of lead on Venus will melt like a block of ice would on Earth.", Climate = "Is around 900F. Even spacecraft cannot withstand the atmosphere for long.", LifeFormDetails = "Zorthraxians, gelatinous cubes, 4 legs, 6 feet in size, 12 hearts, very angry all the time", Population = 12},
+          new Animal { AnimalId = 2, Breed = "Cat", Name = "Stinky", Age = 1, FunFact = "Smells like Drakar Noir. Cuddly, Hairless." },
           
-        //   new Planet { PlanetId = 3, Name = "Mars", FunFact = "Mars has two moons. Mars has frozen water and is seismically active. It has the hightest mountain in our solar system.", Climate = "Mars's atmosphere is primarily formed of carbon dioxide. Temperatures range from -220F to 70F, with an average temperature of -81F. Mars has seasons that cause its frozen water and carbon dioxide caps to shrink and grow.", LifeFormDetails = "Blarbs, Candy bars with delicious nougat centers, on one 20 foot wheel with spinner rims", Population = 10000},
+          new Animal { AnimalId = 3, Breed = "Cat", Name = "Steve", Age = 40, FunFact = "Actually human, but pretends to be a cat. Could use a nice home, psychiatrist, and medication." },
           
-        //   new Planet { PlanetId = 4, Name = "Saturn", FunFact = "Saturn could float in water due to it's gaseous nature. It has 82 moons.", Climate = "Extremely windy with averages temperatures hovering around -285F.", LifeFormDetails = "Recklins, flying recliner-chair like creatures, roughly the size of blue-whales", Population = 5000000},
+          new Animal { AnimalId = 4, Breed = "Dog", Name = "Zorthrax", Age = 6, FunFact = "Poops chocolate. Know how to code in C#. Allergic to water" },
           
-        //   new Planet { PlanetId = 5, Name = "Pluto", FunFact = "It is a dwarf planet, it's half the size of the US.", Climate = "Average temperature is -387F, thin atmosphere made of nitrogen, methane and carbone monoxide", LifeFormDetails = "Plutonians, holograms, exact replicas of Pluto from Disney, loves holograms of earth pizza", Population = 9999999}
-        // );
+          new Animal { AnimalId = 5, Breed = "Dog", Name = "The Dog Formerly Known as Prince", Age = 2, FunFact = "Sings beautifully. Crows like a rooster. Is a delivery driver for Amazon" },
+
+          new Animal { AnimalId = 6, Breed = "Tiger", Name = "Lion", Age = 1, FunFact = "Previous owner was terrible at animal identification. Gluten-free. Flys" }
+        );
     }
   }
 }
