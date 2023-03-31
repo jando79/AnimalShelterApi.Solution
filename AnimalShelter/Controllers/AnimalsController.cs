c83772ce084f6c688b313d0418cd04bf4cf41469
David Jandron <jandron1211@gmail.com>
2023-03-31 10:01:12 -0700

    ADD launchsettings edit and start on controller

---
+++ AnimalShelter/Controllers/AnimalsController.cs
@@ -0,0 +1,22 @@
+// using System.Linq;
+// using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AnimalShelter.Models;
+
+namespace AnimalShelter.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class AnimalsController : ControllerBase
+  {
+    private readonly AnimalShelterContext _db;
+
+    public AnimalsController(AnimalShelterContext db)
+    {
+      _db = db;
+    }
+
+    
+  }
+}
